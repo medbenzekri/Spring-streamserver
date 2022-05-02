d11119bb42a3d4f43f4fafaab86e854c441a03bd
medbenzekri <medbenzekri219@gmail.com>
2022-05-02 19:11:21 +0100

    image test added and working

--- src/test/java/dz/univ/bechar/mda/service/FFmpegServiceTest.java
+++ src/test/java/dz/univ/bechar/mda/service/FFmpegServiceTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,11 +18,20 @@ public class FFmpegServiceTest {
     @Autowired
     FFmpegService service;
     @Test
-    public void givenObjectStream_whenCalculate_duration() throws  Exception{
+    public void givenObjectStream_whenCalculate_duration() {
         Duration realduration
                 = Duration.parse("PT20S");
         Duration duration = service.genrator("Monkeys360.mp4").getduration();
 
         assertThat(duration.getSeconds()).isEqualTo(realduration.getSeconds());
+    }
+    @Test
+    public void givenObjectStream_whenGenerate_Thumbnail() throws IOException {
+       InputStream stream= service.genrator("Monkeys360.mp4").generatethumbnail();
+
+        File targetFile= new File("src/main/resources/image.jpeg");
+        BufferedImage image = ImageIO.read(stream);
+        ImageIO.write(image,"jpeg",targetFile);
+
     }
 }

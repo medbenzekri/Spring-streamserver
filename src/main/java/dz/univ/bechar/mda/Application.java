e52120f1d81bd3fe826c53fb46bdb8f012653c1f
medbenzekri <medbenzekri219@gmail.com>
2022-05-21 16:15:47 +0100

    unnecessary imports removed

--- src/main/java/dz/univ/bechar/mda/Application.java
+++ src/main/java/dz/univ/bechar/mda/Application.java
@@ -2,12 +2,7 @@ package dz.univ.bechar.mda;
 
 import java.util.Arrays;
 
-import dz.univ.bechar.mda.entity.Membership;
-import dz.univ.bechar.mda.entity.Role;
-import dz.univ.bechar.mda.entity.User;
-import dz.univ.bechar.mda.repository.MembershipRepository;
-import dz.univ.bechar.mda.repository.RoleRepository;
-import dz.univ.bechar.mda.repository.UserRepository;
+import dz.univ.bechar.mda.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,22 +12,15 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-	@Autowired
-	private UserRepository repository;
-	@Autowired
-	private MembershipRepository membershipRepository;
-	@Autowired
-	private RoleRepository roleRepository;
 
-
+	@Autowired
+    StorageService service;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		//run for first time
-		// injectData();
+
 		return args -> {
 
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
@@ -46,25 +34,7 @@ public class Application {
 		};
 	}
 
-	public boolean injectData(){
-		try {
-			Role user= new Role("User");
-			Membership freemember= new Membership("Free");
-			roleRepository.save(user);
-			membershipRepository.save(freemember);
-			repository.save(new User("medbenzkeri","12345678","medbenzekri213@gmail.com",user));
-			repository.save(new User("Jack", "y8KbckjS"));
-			repository.save(new User("Chloe", "mQy4Hs6r"));
-			repository.save(new User("Kim", "xn5Jf3F7"));
-			repository.save(new User("David", "9cRNw7bd"));
-			repository.save(new User("Michelle", "b9ZCVXJN"));
-			return true;
-		}catch (Exception e){
-			System.out.println(e);
 
-		}
-		return false;
-	}
 
 
 }
